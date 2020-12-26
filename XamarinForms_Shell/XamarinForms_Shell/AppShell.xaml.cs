2574c51095bf04f11fd6880ae7e8563615cae7cf
Steven <steven0529@gmail.com>
2020-12-27 02:22:41 +0800

    加入專案檔案。

---
+++ XamarinForms_Shell/XamarinForms_Shell/AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace _03.ShellTest
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Shell.SetTabBarIsVisible(this, false);//隱藏頁籤
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://www.tpisoftware.com/");
+        }
+    }
+}
